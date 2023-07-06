8077f713e8b072cffb7e59cda6dd4fef1052609e
Oriol Julián <oriolposada@gmail.com>
2023-07-06 13:38:12 +0200

    Zhiqiang grafos, pruebas de modificacion de P

--- codigo/primer_grafo/p_54/expresiones_equivalentes.py
+++ codigo/primer_grafo/p_54/expresiones_equivalentes.py
@@ -9,35 +9,13 @@ def are_equal(e1, e2, nbits=5):
         for i in range(n):
             vars[f"z_{i}"] = z[i]
 
-        res1 = eval(e1, dict({"P": 27}, **vars))
+        res1 = eval(e1, dict({"P": 54}, **vars))
         res2 = eval(e2, vars)
         if res1 != res2:
             is_wrong = True
             print("Error: ", f"{z}", res1, res2)
     if not is_wrong:
         print("Las expresiones son equivalentes")
-
-
-exp_orig = "\
-5*(1 - z_0)/2 + \
-8*(1 - z_1)/2 + \
-2*(1 - z_2)/2 + \
-7*(1 - z_3)/2 + \
-4*(1 -  z_4)/2 + \
-P * ((1 - z_0)/2 + (1 - z_1)/2 - 1)**2 + \
-P * ((1 - z_0)/2 - (1 - z_2)/2 - (1 - z_3)/2)**2 + \
-P * ((1 - z_1)/2 + (1 - z_2)/2 - (1 - z_4)/2)**2"
-
-exp2 = "\
- 11   * z_0 \
--17.5 * z_1 \
--28   * z_2 \
--17   * z_3 \
-+11.5 * z_4 \
-+13.5 * (- z_0*z_2 + z_1*z_2 + z_2*z_3 - z_2*z_4 + z_0*z_1 - z_0*z_3 \
-         - z_1*z_4) \
-+(13.5*(z_0**2 + z_1**2 + z_2**2) + 6.75*(z_3**2 + z_4**2)) \
-+26.5"
 
 exp_orig_mas_restricc = "\
 5*(1 - z_0)/2 + \
@@ -50,14 +28,19 @@ P * ((1 - z_3)/2 + (1 - z_4)/2 - 1)**2 + \
 P * ((1 - z_0)/2 - (1 - z_2)/2 - (1 - z_3)/2)**2 + \
 P * ((1 - z_1)/2 + (1 - z_2)/2 - (1 - z_4)/2)**2"
 
-exp2_mas_restricc = "11 * z_0 - 17.5 * z_1 - 28 * z_2 - 17 * z_3 + 11.5 * z_4 \
-+ 13.5 * (z_0*z_1 + z_1*z_2 + z_2*z_3 - z_0*z_2 - z_0*z_3 + z_3*z_4 \
-        - z_1*z_4 - z_2*z_4) \
-+ 13.5 * (z_0**2 + z_1**2 + z_2**2 + z_3**2 + z_4**2) \
-+ 26.5 "
+
+exp2_mas_restricc = "\
+24.5*z_0 - 31*z_1 - 55*z_2 - 30.5*z_3 + 25*z_4 + \
+27*z_0*z_1 + 27*z_1*z_2 + 27*z_2*z_3 + 27*z_3*z_4 - 27*z_0*z_2 - 27*z_0*z_3 \
+    - 27*z_1*z_4 - 27*z_2*z_4 + \
+27*z_0**2 + 27*z_1**2 + 27*z_2**2 + 27*z_3**2 + 27*z_4**2 + 40 \
+"
+# exp2_mas_restricc = "11 * z_0 - 17.5 * z_1 - 28 * z_2 - 17 * z_3 + 11.5 * z_4 \
+# + 13.5 * (z_0*z_1 + z_1*z_2 + z_2*z_3 - z_0*z_2 - z_0*z_3 + z_3*z_4 \
+#         - z_1*z_4 - z_2*z_4) \
+# + 13.5 * (z_0**2 + z_1**2 + z_2**2 + z_3**2 + z_4**2) \
+# + 26.5 "
 
 if __name__ == "__main__":
-    print("exp_orig == exp2: ", end="")
-    are_equal(exp_orig, exp2)
     print("exp_orig_mas_restricc == exp2_mas_restricc: ", end="")
     are_equal(exp_orig_mas_restricc, exp2_mas_restricc)
---
+++ codigo/zhiqiang_grafo/expresiones_equivalentes.py
@@ -0,0 +1,52 @@
+import itertools
+import numpy as np
+
+def are_equal(e1, e2, nbits=4, p=20):
+    n = nbits
+    is_wrong = False
+    min_val = np.Infinity
+    min_bits = None
+    for z in itertools.product([-1, 1], repeat=n):
+        vars = dict()
+        for i in range(n):
+            vars[f"z_{i}"] = z[i]
+
+        res1 = eval(e1, dict({"P": p}, **vars))
+        res2 = eval(e2, vars)
+        if res1 != res2:
+            is_wrong = True
+            print("Error: ", f"{z}", res1, res2)
+        if res1 < min_val:
+            min_val = res1
+            min_bits = z
+    if not is_wrong:
+        print("Las expresiones son equivalentes, min=" + str(min_val), min_bits)
+
+exp_orig="\
+3*(1-z_0)/2 + \
+6*(1-z_1)/2 + \
+9*(1-z_2)/2 + \
+1*(1-z_3)/2 + \
+P*((1 - z_0)/2 + (1 - z_1)/2 - 1)**2 + \
+P*((1 - z_2)/2 + (1 - z_3)/2 - 1)**2 + \
+P*((1 - z_0)/2 - (1 - z_2)/2)**2 + \
+P*((1 - z_1)/2 - (1 - z_3)/2)**2 \
+"
+
+exp_20 = "\
+-1.5*z_0 - 3*z_1 - 4.5*z_2 - 0.5*z_3 + \
+10*z_0*z_1 - 10*z_0*z_2 - 10*z_1*z_3 + 10*z_2*z_3 + \
+10*z_0**2 + 10*z_1**2 + 10*z_2**2 + 10*z_3**2 + 9.5 \
+"
+
+exp_40 = "\
+- 1.5*z_0 - 3*z_1 - 4.5*z_2 - 0.5*z_3 + \
+20*z_0*z_1 - 20*z_0*z_2 - 20*z_1*z_3 + 20*z_2*z_3 + \
+20*z_0**2 + 20*z_1**2 + 20*z_2**2 + 20*z_3**2 + 9.5 \
+"
+
+if __name__ == "__main__":
+    print("exp_orig == exp_20: ", end="")
+    are_equal(exp_orig, exp_20, p=20)
+    print("exp_orig == exp_40: ", end="")
+    are_equal(exp_orig, exp_40, p=40)
